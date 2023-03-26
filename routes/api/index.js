9360000f1dc72ffae5238c40cdc030f94290c994
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-03-26 21:24:24 +0700

    restructor project

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const auth = require('../auth')
-
-router.use('/users', require('./user'))
-router.use('/posts', auth.required, require('./post'))
-
-module.exports = router
